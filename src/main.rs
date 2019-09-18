5cdc3e13479925ae361bd2034b967b45d097caaf
Sean Behan <codebam@riseup.net>
2019-09-18 00:00:31 -0400

    Generate user ids

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,11 @@ fn client_connected(ip: SocketAddr, clients: &Arc<Mutex<Vec<Client>>>) -> bool {
     false
 }
 
-fn handle_client(mut stream: TcpStream, clients: Arc<Mutex<Vec<Client>>>) {
+fn handle_client(
+    mut stream: TcpStream,
+    clients: Arc<Mutex<Vec<Client>>>,
+    id: Arc<Mutex<Vec<usize>>>, // stack to keep track of user ids
+) {
     let mut data = [0 as u8; 256]; // using 256 byte buffer
     'connection: while match stream.read(&mut data) {
         // close connection when the client disconnects
@@ -38,8 +42,16 @@ fn handle_client(mut stream: TcpStream, clients: Arc<Mutex<Vec<Client>>>) {
                         println!("client authenticated: {:#?}", c);
                         clients.lock().unwrap().push(c);
 
-                        let user_id = 1;
-                        // TODO: implement user ID
+                        let user_id: usize = match id.lock() {
+                            Ok(mut id) => {
+                                let t = id.len();
+                                let y = id[t - 1];
+                                id.truncate(t - 1);
+                                y
+                            }
+                            Err(_) => 999,
+                            // default user id is 999
+                        };
 
                         stream
                             .write(format!("SUCC {}\n", user_id).as_bytes())
@@ -93,22 +105,33 @@ struct Client {
     username: String,
 }
 
+fn create_user_ids(max_id: usize) -> Vec<usize> {
+    let mut v = Vec::with_capacity(max_id);
+    for i in (0..max_id).rev() {
+        v.push(i);
+    }
+    return v;
+}
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:3333").unwrap();
     let clients = Arc::new(Mutex::new(vec![Client {
         ip: SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 0),
         username: "root".to_string(),
     }]));
+    let max_id = 100;
+    let user_id = Arc::new(Mutex::new(create_user_ids(max_id)));
     // accept connections and process them, spawning a new thread for each one
     println!("Server listening on port 3333");
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
                 let clients = clients.clone();
+                let user_id = user_id.clone();
                 println!("New connection: {}", stream.peer_addr().unwrap());
                 thread::spawn(move || {
                     // connection succeeded
-                    handle_client(stream, clients)
+                    handle_client(stream, clients, user_id)
                 });
             }
             Err(e) => {
