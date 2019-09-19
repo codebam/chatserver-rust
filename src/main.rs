80b8d1fabb60f70f7c1208ad13a356e5e501e308
Sean Behan <contact@seanbehan.dev>
2019-09-19 18:07:23 -0400

    Clients can send messages to each other

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::{Read, Write};
+use std::io::{Read, Write, Error};
 use std::net::{IpAddr, Ipv4Addr, Shutdown, SocketAddr, TcpListener, TcpStream};
 use std::str;
 use std::sync::{Arc, Mutex};
@@ -6,6 +6,40 @@ use std::thread;
 use std::vec::Vec;
 
 const VERSION: &str = "FC1";
+
+fn data_parser(data: String) -> (String, String) {
+    let mut words = data.split_whitespace();
+    let command = words.next().unwrap().to_string();
+    let mut chars = data.chars();
+    for character in chars.by_ref() {
+        if character.is_whitespace() {
+            break;
+        }
+    }
+    let options = chars.as_str().to_string();
+    return (command, options)
+}
+
+fn send_parser(options: String) -> (String, String) {
+    let mut words = options.split(":");
+    let userid = words.next().unwrap().to_string();
+    let mut chars = options.chars();
+    for character in chars.by_ref() {
+        if character == ':' {
+            break;
+        }
+    }
+    let message = chars.as_str().to_string();
+    return (userid, message)
+}
+
+fn send_message(client: &ClientHandler, userid: String, message: String, clients: Arc<Mutex<Vec<Client>>>) -> Result<usize, Error> {
+    let clients_lock = clients.lock().unwrap();
+    let from = clients_lock.get_client_by_ip(client.socket_addr).unwrap();
+    let to = clients_lock.get_client_by_userid(userid).unwrap();
+    let send = format!("RECV {}:{}", from.id, message);
+    to.stream.try_clone().unwrap().write(send.as_bytes())
+}
 
 fn handle_client(
     mut stream: TcpStream,
@@ -24,25 +58,35 @@ fn handle_client(
             break 'connection;
         }
         Ok(_) => {
-            let mut words = str::from_utf8(&data).unwrap().split_whitespace();
-            let command = words.next();
+            let (command, options_string) = data_parser(str::from_utf8(&data).unwrap().to_string());
+            let command = &*command; 
+            // let options = &*options_string;
+            // deref our strings
+
+            let mut words = options_string.split_whitespace();
+
             if !clients.lock().unwrap().connected(client.socket_addr) {
                 match command {
-                    Some("LOGN") => {
-                        let c = Client {
+                    "LOGN" => {
+                        let mut c = Client {
+                            stream: stream.try_clone().unwrap(),
                             ip: client.socket_addr,
                             username: words.next().unwrap().to_string(),
+                            id: 999,
                             version: words.next().unwrap().to_string(),
                         };
                         if VERSION == c.version {
-                            println!("client authenticated: {:#?}", c);
-                            clients.lock().unwrap().push(c);
-
-                            let user_id: usize = match id.lock() {
+                            let user_id = match id.lock() {
                                 Ok(mut id) => id.pop().unwrap(),
                                 Err(_) => 999,
                                 // default user id is 999
                             };
+                            c.id = user_id;
+
+                            // set the correct user id if we're letting the client connect
+
+                            println!("client authenticated: {:#?}", c);
+                            clients.lock().unwrap().push(c);
 
                             stream
                                 .write(format!("SUCC {}\n", user_id).as_bytes())
@@ -52,23 +96,21 @@ fn handle_client(
                             break 'connection;
                         } // only accept client if they're using the same version as us
                     } // client authentication
-                    Some(_) => {
+                    _ => {
                         println!("closing connection. invalid command");
                         break 'connection;
                     }
-                    // invalid command
-                    None => {
-                        println!("closing connection. no more data2");
-                        break 'connection;
-                    } // no more data
                 }
             } // don't allow clients to authenticate more than once
 
             if clients.lock().unwrap().connected(client.socket_addr) {
                 match command {
-                    Some("") => {} // auth new client
-                    Some(_) => {}
-                    None => {}
+                    "GDBY" => break 'connection, // graceful disconnect
+                    "SEND" => {
+                        let (userid, message) = send_parser(options_string);
+                        let _ = send_message(&client, userid, message, clients.clone());
+                    }
+                    _ => {}
                 }
             } else {
                 println!("closing connection. client didn't login");
@@ -94,7 +136,9 @@ fn handle_client(
 #[derive(Debug)]
 struct Client {
     ip: SocketAddr,
+    stream: TcpStream,
     username: String,
+    id: usize,
     version: String,
 }
 
@@ -130,23 +174,51 @@ impl PartialEq for Client {
 
 trait ClientMethods {
     fn connected(&self, ip: SocketAddr) -> bool;
+    fn get_client_by_username(&self, username: String) -> Option<&Client>;
+    fn get_client_by_userid(&self, username: String) -> Option<&Client>;
+    fn get_client_by_ip(&self, ip: SocketAddr) -> Option<&Client>;
 }
 
 impl ClientMethods for Vec<Client> {
-    fn connected(&self, ip: SocketAddr) -> bool {
+    fn get_client_by_ip(&self, ip: SocketAddr) -> Option<&Client> {
         for c in self.iter() {
             if c.ip == ip {
-                return true;
+                return Some(c);
             } // client found
         }
-        false
+        None
+    }
+
+    fn connected(&self, ip: SocketAddr) -> bool {
+        match self.get_client_by_ip(ip) {
+            Some(_) => true,
+            None => false
+        }
+    }
+    fn get_client_by_username(&self, username: String) -> Option<&Client> {
+        for c in self.iter() {
+            if c.username == username {
+                return Some(c);
+            }
+        }
+        None
+    }
+    fn get_client_by_userid(&self, user_id: String) -> Option<&Client> {
+        for c in self.iter() {
+            if c.id.to_string() == user_id {
+                return Some(c);
+            }
+        }
+        None
     }
 }
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:3333").unwrap();
     let clients = Arc::new(Mutex::new(vec![Client {
+        stream: TcpStream::connect("127.0.0.1:3333").unwrap(),
         ip: SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 0),
+        id: 0,
         username: "root".to_string(),
         version: VERSION.to_string(),
     }]));
